31c5c9761e1b507d52f64ea64ba2bb6b63f1f176
matinGhaffari <52476475+matin-g@users.noreply.github.com>
2022-11-23 20:14:42 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,7 @@
-import os
-os.system(run.sol)
+# I was told to submit this as empty file by TA (as well as an empty submission.json and test folder & data)
+# since we cannot run solidity locally since we deploy/test our contract online using the goerli test Ethereum
+# network (https://goerli.etherscan.io/address/0x478805e928c9449914ae22da20dfdf94841e0d8e#code) also because
+# our project doesn't rely on stored data in any way.
+# 
+# The Professor said it is okay to not submit this assignment in our case in the following post:
+# https://edstem.org/us/courses/28947/discussion/2202683
